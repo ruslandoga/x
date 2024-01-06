0969c2be87e2cc8dccaf782ad4efdaa77263375e
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2024-01-06 17:43:07 +0900

    oban and ch

--- mix.exs
+++ mix.exs
@@ -50,7 +50,9 @@ defmodule X.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:oban, "~> 2.16"},
+      {:ecto_ch, "~> 0.3.1"}
     ]
   end
 
